ba410874df3ea1fd90910ac07ce1458653f8e9a1
Prince <princek2530@gmail.com>
2022-08-04 18:32:10 +0530

    power of 2

---
+++ src/com/bridgelabz/powerof2.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class powerof2 {
+    public static void main(String[] args) {
+        int p;
+        int i = 0;
+        int power = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to calculate of its power");
+        p = sc.nextInt();
+        while (i <= p) {
+            System.out.println(i + " " + power);
+            power = 2 * power;
+            i = i + 1;
+        }
+    }
+}
