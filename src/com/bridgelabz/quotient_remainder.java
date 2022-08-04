040f0ee99d1d265e5d6242e6cf63da96439a8256
Prince <princek2530@gmail.com>
2022-08-04 13:25:08 +0530

    quotient_remainder

---
+++ src/com/bridgelabz/quotient_remainder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class quotient_remainder {
+    public static void main(String[] args) {
+        int dividend = 25;
+        int divisor = 4;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("quotient: " + quotient);
+        System.out.println("remainder: " + remainder);
+    }
+}
